77a7336e1d7dff77181bf772fd2b85872df6b63f
Frelon <lixuanliming@gmail.com>
2019-07-23 21:22:06 +0800

    h

---
+++ fu.js
@@ -0,0 +1,6 @@
+function close(){
+    var dsr=document.getElementById("erer");
+    var de=document.getElementById("closee");
+    dsr.style.visibility='hidden'
+    de.style.visibility='hidden'
+}
