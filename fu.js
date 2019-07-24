e60c841229617867396c123210e7952c4a26d0e9
Frelon <lixuanliming@gmail.com>
2019-07-24 20:03:53 +0800

    update

--- fu.js
+++ fu.js
@@ -1,6 +1,114 @@
-function close(){
-    var dsr=document.getElementById("erer");
-    var de=document.getElementById("closee");
-    dsr.style.visibility='hidden'
-    de.style.visibility='hidden'
+function changeBlock(){
+    var f = document.getElementById("hek")
+    f.style.display="block";
 }
+
+function changeNone(){
+    var f = document.getElementById("hek")
+    f.style.display="none";
+}
+
+
+
+
+
+function changeBlockA(){
+    var f = document.getElementById("hekA")
+    f.style.display="block";
+}
+
+function changeNoneA(){
+    var f = document.getElementById("hekA")
+    f.style.display="none";
+}
+
+
+
+
+function changeBlockB(){
+    var f = document.getElementById("hekB")
+    f.style.display="block";
+}
+
+function changeNoneB(){
+    var f = document.getElementById("hekB")
+    f.style.display="none";
+}
+
+
+
+
+function changeBlockC(){
+    var f = document.getElementById("hekC")
+    f.style.display="block";
+}
+
+function changeNoneC(){
+    var f = document.getElementById("hekC")
+    f.style.display="none";
+}
+
+
+
+
+
+
+function changeXiaomi(){
+    var fr =document.getElementById("demo")
+    fr.style.display="block"
+    var es=document.getElementById("class")
+    es.style.background=("orange")
+}
+
+function changeRedmi(){
+    var fr =document.getElementById("demo")
+    fr.style.display="none"
+    var es=document.getElementById("class")
+    es.style.background=("whitesmoke")
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function changeXiaomiA(){
+    var fr =document.getElementById("demoA")
+    fr.style.display="block"
+    var es=document.getElementById("classA")
+    es.style.background=("orange")
+}
+
+function changeRedmiA(){
+    var fr =document.getElementById("demoA")
+    fr.style.display="none"
+    var es=document.getElementById("classA")
+    es.style.background=("whitesmoke")
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function changeXiaomiB(){
+    var fr =document.getElementById("demoB")
+    fr.style.display="block"
+    var es=document.getElementById("classB")
+    es.style.background=("orange")
+}
+
+function changeRedmiB(){
+    var fr =document.getElementById("demoB")
+    fr.style.display="none"
+    var es=document.getElementById("classB")
+    es.style.background=("whitesmoke")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
